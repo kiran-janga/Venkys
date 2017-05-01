385073df4b7ac61b5c9235dff3f69d7ec6385c89
Saikiran <janga.sai@iitgn.ac.in>
2017-05-01 14:15:36 +0530

    First 3 Screens

---
+++ app/src/main/java/android/com/venkys/EndScreenActivity.java
@@ -0,0 +1,23 @@
+package android.com.venkys;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by Saikiran on 5/1/2017.
+ */
+
+public class EndScreenActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.end_screen);
+
+
+    }
+
+}
---
+++ app/src/main/java/android/com/venkys/SplashScreenActivity.java
@@ -0,0 +1,33 @@
+package android.com.venkys;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by Saikiran on 5/1/2017.
+ */
+
+public class SplashScreenActivity extends Activity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
